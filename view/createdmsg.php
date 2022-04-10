63864dbddc7c4fcfab1fa9427b9974b91d94f57c
rbaihis <seifallah.rbaihi@esprit.tn>
2022-04-10 20:45:12 +0100

    cleaning

--- view/createdmsg.php
+++ view/createdmsg.php
@@ -9,17 +9,24 @@ if( $_SESSION['info_msg']["create_msg"]===true )
     
 ?>
 
-
 <html>
-    <head></head>
+    <head>
+
+        <style>
+ *{ transition: all 0.6s; } html { height: 100%; } body{ font-family: 'Lato', sans-serif;   color: #888;  margin: 0; } #main{ display: table; width: 100%; height: 100vh; text-align: center; }  .fof{ display: table-cell;	  vertical-align: middle; } .fof h1{ font-size: 50px; display: inline-block; padding-right: 12px; animation: type .5s alternate infinite;} @keyframes type{ from{box-shadow: inset -3px 0px 0px #888;} to{box-shadow: inset -3px 0px 0px transparent;}}
+        </style>
+    </head>
     <body>
-        <p>Thx for joining our family .</p>
-        <h4>Congratulations!</h4> account created successfully *_* </h4>
-        <h4> please <a href="../view/login.php" > login</a> </h4>
+
+
+    <div id="main">
+    	<div class="fof">
+        		<h1> ACCOUNT CREATE</h1>
+                <h3>Congratulations!  we are glad to have you *_* </h3>
+    	         <h4>please <a href="../view/login.php" > login</a> </h4>
         
     </body>
-    </html>
-    
+</html>
 
 <?php 
 
