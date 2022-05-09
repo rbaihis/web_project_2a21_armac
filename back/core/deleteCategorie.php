ff4952ec1c5e91ad0e56216a7474596a95b0b66b
rbaihis <seifallah.rbaihi@esprit.tn>
2022-05-09 03:20:00 +0100

    deleting correptud files

--- back/core/deleteCategorie.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include('../controller_back/categC.php');
-$categorie_C=new categC();
-
-$type=$_POST["type"];
-$categorie_C->supprimerCategorie($type,$categorie_C->conn);
-header('Location: ../view_back/categorie.php');
-?>
--- back/view_back/deleteProduit.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../controller_back/produitC.php';
-$produitC=new produitC();
-$produitC->supprimerProduit($_GET["id"]);
-header('Location:afficherproduit.php');
-?>
-
-
